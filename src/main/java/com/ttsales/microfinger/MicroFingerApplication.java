f338270bfe564ed3cbd81801c740e9983178cc38
liyi <liyi.9ban@gmail.com>
2016-03-03 10:19:06 +0800

    Initial commit

---
+++ src/main/java/com/ttsales/microfinger/MicroFingerApplication.java
@@ -0,0 +1,12 @@
+package com.ttsales.microfinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroFingerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroFingerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ttsales/microfinger/MicroFingerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ttsales.microfinger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MicroFingerApplication.class)
+public class MicroFingerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
