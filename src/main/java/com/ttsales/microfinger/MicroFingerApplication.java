dc6a4e8f5f286db2f663b91c2d6500ebd7a15c0a
zwt1990 <791431798@qq.com>
2016-03-08 00:05:05 +0800

    Update MicroFingerApplication.java

--- src/main/java/com/ttsales/microfinger/MicroFingerApplication.java
+++ src/main/java/com/ttsales/microfinger/MicroFingerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroFingerApplication {
 
 	public static void main(String[] args) {
+		String a="test";
 		SpringApplication.run(MicroFingerApplication.class, args);
 	}
 }
